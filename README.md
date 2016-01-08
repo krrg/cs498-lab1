ef48e676139deebf243cc3b7c9268696eaf8bec3
Ken <krr428@megaman.cs.byu.edu>
2016-01-07 19:27:43 -0700

    Added unit tests

--- README.md
+++ README.md
@@ -4,14 +4,16 @@
 
 In CS 360 you used OS level polling routines like `select` or `epoll` to write an HTTP server.  In this lab, we will move up a level of abstraction and use a cross-platform library called `libuv` to write a simple key-value store server, after the spirit of Redis.  
 
-`libuv` is the same library that the popular Node.js runtime uses under the hood.  Since `libuv` is a cross-platform library, it hides the operating-system specific polling mechanisms. 
+`libuv` is the same library that the popular Node.js runtime uses under the hood.  Since `libuv` is a cross-platform library, it hides the operating-system specific polling mechanisms.
 
 The [Redis key-value store](redis.io) is a popular key-value store server running on many popular websites.  Although there are a lot of Redis commands, we will only implement the simplest of commands for this project (the focus is on the asynchronicity, not the details of writing a good key-value store).  To keep things really simple, we will only require you to implement a simple version of `GET`, `SET`, and `DEL`.  
 
 The syntax for the simple version of these commands looks like:
 
 `GET [key]`
+
 `SET [key] [value]`
+
 `DEL [key]`
 
 A command is terminated by a newline (`\n`) character.  
@@ -24,4 +26,5 @@ You may either use C or the Python package that provides bindings to `libuv`,  c
 To get up to speed on `libuv` and some of the architecture around it, you may find the following useful.
 
 https://nikhilm.github.io/uvbook/
+
 https://youtu.be/nGn60vDSxQ4
--- testclient.js
+++ testclient.js
@@ -1,26 +1,177 @@
 'use strict'
 
 var net = require('net');
-const NUM_SOCKS = 100;
+var Promise = require('bluebird');
+var mocha = require('mocha');
+var child_process = require('child_process');
+var assert = require('assert');
+var _ = require('lodash');
 
-var numDone = 0;
+Promise.promisifyAll(net.Socket.prototype)
 
-for (let i = 0; i < NUM_SOCKS; i++) {
 
-  let sock = new net.Socket();
-  sock.connect(6379, 'localhost', function() {
-    console.log(`Client ${i} is holding connection.`);
+class LineListener {
 
-    sock.write(`SET KEY-${i} VAL-${i}\n`, function() {
-      // When it is done writing.
-      numDone += 1;
-      console.log(`${numDone} sockets complete.`);
-      if (numDone === NUM_SOCKS) {
-        console.log("All sockets are completed.");
-      }
+  /* Expects a promisified socket! */
+  constructor(sock) {
+    this.sock = sock;
+    this.completedLines = [];
+    this.receivingLine = "";
+    this.readLineQueue = [];
 
-      sock.end();
-      sock.destroy();
-    });
-  })
+    let self = this;
+
+    this.sock.on('data', (data) => {
+      data.toString().split('').forEach((c) => {
+        if (c === '\n') {
+          self.completedLines.push(self.receivingLine);
+          self.receivingLine = "";
+          if (self.readLineQueue.length > 0) {
+            self.readLineQueue.shift()();
+          }
+        } else {
+          self.receivingLine += c;
+        }
+      });
+    })
+  }
+
+  writeLine(text) {
+    return this.sock.writeAsync(text + "\n");
+  }
+
+  readLine(text) {
+    let self = this;
+
+    if (self.completedLines.length > 0) {
+      return Promise.resolve(self.completedLines.shift());
+    } else {
+      return new Promise((resolve, reject) => {
+        self.readLineQueue.push(() => {
+          resolve(self.completedLines.shift());
+        });
+      });
+    }
+
+  }
+
 }
+
+
+describe("Simple key-value store server", function() {
+
+  let subprocess = null;
+
+  beforeEach(Promise.coroutine(function* () {
+    subprocess = child_process.spawn('./env/bin/python', ['server.py']);
+  }));
+
+  afterEach(() => {
+    if (subprocess) {
+      subprocess.kill();
+      subprocess = null;
+    }
+  })
+
+  it("GET returns `None` if key does not exist", Promise.coroutine(function* () {
+    let sock = new net.Socket();
+    yield sock.connectAsync(6379, 'localhost');
+    let wrap = new LineListener(sock);
+
+    wrap.writeLine("GET nosuchkey");
+    let result = yield wrap.readLine();
+
+    assert(result.trim() === 'None');
+  }))
+
+  it("executes SET", Promise.coroutine(function* () {
+    let sock = new net.Socket();
+    yield sock.connectAsync(6379, 'localhost');
+    let wrap = new LineListener(sock);
+
+    wrap.writeLine("SET anewkey anewvalue");
+    let result = yield wrap.readLine();
+    assert(result.trim() === '1');
+  }));
+
+  it("can retrieve a previously SET value", Promise.coroutine(function* () {
+    let sock = new net.Socket();
+    yield sock.connectAsync(6379, 'localhost');
+    let wrap = new LineListener(sock);
+
+    yield wrap.writeLine("SET somekey somevalue");
+    let result = yield wrap.readLine();
+
+    yield wrap.writeLine("GET somekey");
+    result = yield wrap.readLine();
+    assert(result.trim() === 'somevalue');
+  }));
+
+  it("can delete a previously SET value", Promise.coroutine(function* () {
+    let sock = new net.Socket();
+    yield sock.connectAsync(6379, 'localhost');
+    let wrap = new LineListener(sock);
+
+    yield wrap.writeLine("SET somekey somevalue");
+    let result = yield wrap.readLine();
+
+    yield wrap.writeLine("DEL somekey");
+    result = yield wrap.readLine();
+
+    yield wrap.writeLine("GET somekey");
+    result = yield wrap.readLine()
+    assert(result.trim() === 'None');
+
+  }))
+
+  it("maintains two open connections and respond to them", Promise.coroutine(function* () {
+    let sock1 = new net.Socket();
+    yield sock1.connectAsync(6379, 'localhost');
+    let wrap1 = new LineListener(sock1);
+
+    let sock2 = new net.Socket();
+    yield sock2.connectAsync(6379, 'localhost');
+    let wrap2 = new LineListener(sock2);
+
+    // Notice the wrap2 issues the SET, and then holds its connection open.
+    yield wrap2.writeLine("SET firstKey firstValue");
+    yield wrap1.writeLine("GET firstKey");
+    let result = yield wrap1.readLine();
+
+    assert(result.trim() === 'firstValue');
+  }))
+
+  it("maintains multiple open connections", Promise.coroutine(function* () {
+    const NUM_SOCKETS = 250;
+
+    let pendingSockets = _.range(NUM_SOCKETS).map(Promise.coroutine(function*(){
+      let sock = new net.Socket();
+      yield sock.connectAsync(6379, 'localhost')
+      return sock;
+    }))
+
+    let sockets = yield Promise.all(pendingSockets);
+
+    let wrappers = sockets.map((s) => {
+      return new LineListener(s);
+    });
+
+    let expectedResponses = _.zipObject(_.range(NUM_SOCKETS), _.range(NUM_SOCKETS).reverse());
+
+    yield Promise.all(wrappers.map((wrapper, index) => {
+      let key = `${index}`;
+      let value = `${expectedResponses[key]}`
+      wrapper.writeLine(`SET ${value} ${value}`);
+      return wrapper.readLine();
+    }))
+
+    yield Promise.all(wrappers.map(Promise.coroutine(function* (wrapper, index) {
+      var key = `${index}`;
+      wrapper.writeLine(`GET ${key}`);
+      let result = yield wrapper.readLine();
+      assert(result.trim() === key);
+    })));
+
+  }));
+
+});
